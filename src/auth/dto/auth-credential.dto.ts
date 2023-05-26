48a94ce3e00b9bf38442dc0dfd51981e98b94e1e
mck0324 <dch3030@naver.com>
2023-05-26 10:56:56 +0900

    회원가입시 유효성체크

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,9 +1,17 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     @IsNotEmpty()
     username: string;
 
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     @IsNotEmpty()
+    //영어랑 숫자만 가능한 유효성 체크
+    @Matches(/^[a-zA-Z0-9]*$/, {message: "비밀번호는 영어와 숫자만 허용됩니다."})
     password: string;
 }
