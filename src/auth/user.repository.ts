fa3c7527c5f9947b0ec4be2e6df458d3dcf88fe0
mck0324 <dch3030@naver.com>
2023-05-26 11:29:27 +0900

    회원가입시 유저네임 중복확인

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,15 +2,24 @@ import { Repository } from "typeorm";
 import { User } from "./user.entity";
 import { CustomRepository } from "src/typeorm-ex.decorator";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
-        const user = this.create({
-            username,
-            password
-        });
-        await this.save(user);
+        
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        try {
+            await user.save();
+        } catch (error) {
+            if(error.code === '23505') {
+                throw new ConflictException('이미 존재하는 유저입니다.');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     } 
 }
