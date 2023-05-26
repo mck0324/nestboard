7ea148e3b7d6a80f22c6518cf2f5bb9b06cfd022
mck0324 <dch3030@naver.com>
2023-05-26 14:11:51 +0900

    회원가입시 비밀번호 암호화

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,17 +3,17 @@ import { User } from "./user.entity";
 import { CustomRepository } from "src/typeorm-ex.decorator";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcryptjs';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
-        
-        const user = new User();
-        user.username = username;
-        user.password = password;
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password,salt);
+        const user = this.create({username,password:hashedPassword});
         try {
-            await user.save();
+            await this.save(user);
         } catch (error) {
             if(error.code === '23505') {
                 throw new ConflictException('이미 존재하는 유저입니다.');
