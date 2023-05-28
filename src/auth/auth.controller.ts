2618564de172f676a329f487936678dcca586d7d
mck0324 <dch3030@naver.com>
2023-05-28 18:59:03 +0900

    커스텀 데코레이터 생성해보기!

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,8 @@ import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from './user.entity';
+import { GetUser } from './get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -21,7 +23,7 @@ export class AuthController {
 
     @Post('/test')
     @UseGuards(AuthGuard())
-    test(@Req() req) {
-        console.log('req',req );
+    test(@GetUser() user: User) {
+        console.log('user',user );
     }
 }
